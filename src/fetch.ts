9c4cf58f1e97bfd6367f4e7c7043d84ce8556ba6
Askar Yusupov <devex.soft@gmail.com>
2024-09-28 10:33:58 +0500

    feat: init

---
+++ src/fetch.ts
@@ -0,0 +1,18 @@
+import fs from "fs";
+import path from "path";
+
+const url = "https://duckduckgo.com/bang.js";
+
+function saveAsJSON(filename: string, data: any) {
+  const content = JSON.stringify(data, null, 2);
+  fs.writeFileSync(filename, content);
+  console.log(`Data saved to ${filename}`);
+}
+async function fetchAndSaveBangs() {
+  const response = await fetch(url);
+  const json = await response.json();
+  const filename = path.resolve(__dirname, "bangs.json");
+  saveAsJSON(filename, json);
+}
+
+fetchAndSaveBangs();
